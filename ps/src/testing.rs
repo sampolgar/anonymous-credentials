90f1879ecf73d1bc815bdf8cc6314b1788156936
spol2078 <sampolgar@gmail.com>
2024-07-18 10:06:09 +1000

    ps working

--- ps/src/testing.rs
+++ ps/src/testing.rs
@@ -1,28 +1,9 @@
-mod testing;
-
-use std::ops::{Add, Mul};
-
 use ark_bls12_381::{Bls12_381, Fr, G1Affine, G2Affine};
 use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup};
 use ark_std::UniformRand;
 // Type aliases for clarity
 type G1A = G1Affine;
 type G2A = G2Affine;
-
-struct SecretKey {
-    x: Fr,
-    y: Fr,
-}
-
-struct PublicKey {
-    x: G2A,
-    y: G2A,
-}
-
-struct Signature {
-    sigma_1: G1A,
-    sigma_2: G1A,
-}
 
 pub fn compare_pairings(g1_1: &G1A, g2_1: &G2A, g1_2: &G1A, g2_2: &G2A) -> bool {
     let pairing1 = Bls12_381::pairing(g1_1, g2_1);
@@ -39,47 +20,6 @@ pub fn g1_generator() -> G1A {
 // Function to get the G2 generator
 pub fn g2_generator() -> G2A {
     G2A::generator()
-}
-
-fn keygen() -> (SecretKey, PublicKey) {
-    let mut rng = ark_std::test_rng();
-    let g2 = g2_generator();
-
-    let x = Fr::rand(&mut rng);
-    let y = Fr::rand(&mut rng);
-
-    let sk = SecretKey { x, y };
-    let pk = PublicKey {
-        x: g2.mul(x).into_affine(),
-        y: g2.mul(y).into_affine(),
-    };
-
-    (sk, pk)
-}
-
-// sk in Fp, pk in g2, Signature in g1
-// sigma_2 = h^x+y*m
-// h.mul(sk.x.into_repr()).add(&g1.mul(sk.y.into_repr()).mul(message.into_repr()));
-fn sign(sk: &SecretKey, message: &Fr) -> Signature {
-    let mut rng = ark_std::test_rng();
-    let h = G1A::rand(&mut rng);
-
-    let sigma_1 = h;
-    let x = sk.x;
-    let y = sk.y;
-    let exponent = x + y * message;
-    let sigma_2 = h.mul(exponent).into_affine();
-    Signature { sigma_1, sigma_2 }
-}
-
-// e(sigma_1, pk.x, pk.y ^m) = e(sigma_2, g_tilde)
-fn verify(pk: &PublicKey, message: &Fr, signature: &Signature) -> bool {
-    let g1 = g1_generator();
-    let g2 = g2_generator();
-
-    let lhs_2 = pk.y.mul(message).add(pk.x).into_affine();
-
-    compare_pairings(&signature.sigma_1, &lhs_2, &signature.sigma_2, &g2)
 }
 
 #[cfg(test)]
@@ -127,16 +67,5 @@ mod tests {
 
         // Test pairing equality: e(a*G1, b*G2) = e(G1, ab*G2)
         assert!(compare_pairings(&g1_mul1, &g2_mul2, &g1, &g2_mul1_2));
-    }
-
-    #[test]
-    fn test_ps1() {
-        let (sk, pk) = keygen();
-        let mut rng = ark_std::test_rng();
-
-        let message = Fr::rand(&mut rng);
-        let signature = sign(&sk, &message);
-        let is_valid = verify(&pk, &message, &signature);
-        println!("Signature validity: {}", is_valid);
     }
 }
