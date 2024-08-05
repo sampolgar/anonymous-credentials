9d4da6607742f105015a4eb345a114ae7e0031be
spol2078 <sampolgar@gmail.com>
2024-08-05 18:44:13 +1000

    selective disclosure

--- ps/src/signature.rs
+++ ps/src/signature.rs
@@ -1,10 +1,12 @@
 use crate::keygen;
 use schnorr::schnorr::SchnorrProtocol;
+use schnorr::schnorr_pairing::{SchnorrCommitmentPairing, SchnorrProtocolPairing};
+use utils::helpers::Helpers;
 use utils::pairing::PairingCheck;
 use utils::pairs::PairingUtils;
 
 use ark_ec::pairing::{Pairing, PairingOutput};
-use ark_ec::{AffineRepr, CurveGroup, VariableBaseMSM};
+use ark_ec::{AffineRepr, CurveGroup, Group, VariableBaseMSM};
 use ark_ff::UniformRand;
 use ark_std::{
     ops::{Mul, Neg},
@@ -20,11 +22,11 @@ pub struct Signature<E: Pairing> {
     pub sigma2: E::G1Affine,
 }
 
-pub struct SignatureMessages<E: Pairing> {
-    pub messages: Vec<E::ScalarField>,
-    pub t: E::ScalarField,
-    pub tt: E::ScalarField,
-}
+// pub struct SignatureMessages<E: Pairing> {
+//     pub messages: Vec<E::ScalarField>,
+//     pub t: E::ScalarField,
+//     pub tt: E::ScalarField,
+// }
 
 impl<E: Pairing> Signature<E> {
     //
@@ -101,6 +103,76 @@ impl<E: Pairing> Signature<E> {
 
         let multi_pairing = E::multi_pairing([a, c], [b, d]);
         multi_pairing.0.is_one()
+    }
+
+    /// Process:
+    /// 1. Randomize the signature
+    /// 2. Create Schnorr proof for hidden messages and randomization factor
+    /// 3. Compute signature commitment in GT
+    /// 4. Return proof containing randomized signature, Schnorr proof, and disclosed messages
+    pub fn prove_selective_disclosure<R: Rng>(
+        &self,
+        pk: &keygen::PublicKey<E>,
+        messages: &[E::ScalarField],
+        disclosed_indices: &[usize],
+        rng: &mut R,
+    ) -> SelectiveDisclosureProof<E> {
+        let r = E::ScalarField::rand(rng);
+        let t = E::ScalarField::rand(rng);
+        let proof_length = disclosed_indices.len() + 1; //t + m for undisclosed m
+        let randomized_sig = self.randomize_for_pok(&r, &t);
+
+        // prepare bases and exponents for proof
+        let bases_g1 =
+            Helpers::copy_point_to_length_g1::<E>(randomized_sig.sigma1.clone(), &proof_length);
+        let bases_g2 = Helpers::add_affine_to_vector::<E::G2>(&pk.g2, &pk.y_g2);
+
+        let mut exponents = vec![t];
+        for (i, m) in messages.iter().enumerate() {
+            if !disclosed_indices.contains(&i) {
+                exponents.push(*m);
+            }
+        }
+
+        // generate blindings and commitment to blindings for messages size of hidden messages
+        let schnorr_commitment = SchnorrProtocolPairing::commit::<E, _>(&bases_g1, &bases_g2, rng);
+
+        let signature_commitment = Helpers::compute_gt::<E>(
+            &[
+                randomized_sig.sigma2,
+                randomized_sig.sigma1.into_group().neg().into_affine(),
+            ],
+            &[pk.g2, pk.x_g2],
+        );
+
+        SelectiveDisclosureProof {
+            randomized_sig,
+            schnorr_commitment,
+            signature_commitment,
+            disclosed_indices: disclosed_indices.to_vec(),
+            disclosed_messages: disclosed_indices.iter().map(|&i| messages[i]).collect(),
+        }
+    }
+}
+
+pub struct SelectiveDisclosureProof<E: Pairing> {
+    randomized_sig: Signature<E>,
+    schnorr_commitment: SchnorrCommitmentPairing<E>,
+    signature_commitment: PairingOutput<E>,
+    disclosed_indices: Vec<usize>,
+    disclosed_messages: Vec<E::ScalarField>,
+}
+
+/// Verifies the pairing equation for the selective disclosure proof
+///
+/// Process:
+/// 1. Compute LHS: e(σ₂', g₂)
+/// 2. Compute RHS: e(σ₁', X) * ∏ᵢ₍ᵈᵢₛcˡₒₛₑᵈ₎ e(σ₁', Yᵢ)^mᵢ
+/// 3. Implicitly verify undisclosed messages (handled by Schnorr proof)
+/// 4. Check if LHS == RHS
+impl<E: Pairing> SelectiveDisclosureProof<E> {
+    fn verify_signature_commitment(&self, pk: &keygen::PublicKey<E>) -> bool {
+        //Implement
     }
 }
 
